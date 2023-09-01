d610de8c7c35d09f8f0749596fcff23ff9c33617
migo1 <ertmigot@gmail.com>
2023-09-01 17:18:27 +0300

    add-basic-ui

---
+++ main.rb
@@ -0,0 +1,37 @@
+require_relative 'app'
+
+app = App.new
+
+puts "Welcome to School Library App! \n\n"
+
+loop do
+  puts 'Choose an option:'
+  puts '1 - List all books'
+  puts '2 - List all people'
+  puts '3 - Create a person'
+  puts '4 - Create a book'
+  puts '5 - Create a rental'
+  puts '6 - List all rentals for a given person id'
+  puts '7 - Exit'
+  option = gets.chomp
+
+  case option
+  when '1'
+    app.list_books
+  when '2'
+    app.list_persons
+  when '3'
+    app.create_person
+  when '4'
+    app.create_book
+  when '5'
+    app.create_rental
+  when '6'
+    app.list_rentals_by_person
+  when '7'
+    print 'Thank you for using this app!'
+    break
+  end
+
+  puts "\n"
+end
